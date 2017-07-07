8c2d4d78112419a9f84406f145f0490a7954d2e0
rob Bertram <rob Bertram>
2017-07-06 23:23:53 -0500

    first

---
+++ BreWeb.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var express = require ('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/static/indexYoga.html');
+});
+
+
+app.listen(2999);
+console.log("Running...")
